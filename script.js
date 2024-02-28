26aefae38ee41246381bef27f6a00a463a41bc99
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-28 16:20:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+window.addEventListener('resize',function(){
+	const width = window.innerWidth;
+	const height = window.innerHeight;
+	const size = document.querySelector('#sizeInfo');
+	size.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+});
