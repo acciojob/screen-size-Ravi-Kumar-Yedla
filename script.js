5bfd1011708552461f6dbb49799268f880b1a671
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-29 09:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 window.addEventListener('resize',function(){
 	const width = window.innerWidth;
 	const height = window.innerHeight;
-	const size = document.querySelector('#sizeInfo');
-	size.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
-});
+	const size =document.getElementById('sizeInfo');
+	if(size){
+	size.innerHTML = `<h1>Width: ${width}px and Height: ${height}px</h1>`;
+	}
+	});
